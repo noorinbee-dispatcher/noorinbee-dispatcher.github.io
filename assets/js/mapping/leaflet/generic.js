5b49617ed69b3459cd3d3fb9f9f3e29c23de1438
apatchofnettles <apatchofnettles@gmail.com>
2020-12-09 14:53:33 +1100

    Leaflet structures introduced

---
+++ assets/js/mapping/leaflet/generic.js
@@ -0,0 +1,209 @@
+
+//////////////////////////////////////////////////////////////
+
+// http://services.land.vic.gov.au/catalogue/publicproxy/guest/dv_geoserver/datavic/ows?
+// https://services.land.vic.gov.au/catalogue/publicproxy/guest/dv_geoserver/wfs?
+// http://services.land.vic.gov.au/catalogue/publicproxy/guest/dv_geoserver/wms?
+// http://www.bom.gov.au/climate/outlooks/mapcache
+// https://base.maps.vic.gov.au/service?
+// https://services.land.vic.gov.au/DELWPmaps/DFW/index.html
+// https://www.openstreetmap.org/
+
+// typeName: 'datavic:FORESTS_OG100', 
+//, ,datavic:FORESTS_OG100 datavic:FLORAFAUNA1_NV2005_EVCBCS_9_1',datavic:FORESTS_RECWEB_POI
+// CROWNLAND_PLM25_COASTAL_RES
+// CROWNLAND_PLM25_COMM_USE_AREA
+// ??? datavic:CROWNLAND_PLM25_FOREST_PARK
+// ?showgrounds? datavic:CROWNLAND_PLM25_NPA_SHED_4
+// datavic:CROWNLAND_PLM25_NATURAL_FEAT
+// datavic:CROWNLAND_PLM25_NATURE_CONSERV
+// datavic:CROWNLAND_PLM25_WILDERNESS
+// datavic:CROWNLAND_PLM25_ALPINE_RESORT
+// datavic:FORESTS_RECWEB_TRACK
+// datavic:FORESTS_RECWEB_POI
+// datavic:FORESTS_RECWEB_HISTORIC_RELIC
+// +'datavic:FORESTS_RECWEB_POI,'
+
+//////////////////////////////////////////////////////////////
+
+var _globalMap = null;
+var _globalControl = null;
+var _globalLegend = null;
+var _legendList = [];
+var _focusLayer = null;
+
+function connectMap(theMap) {
+    _globalMap = theMap;
+}
+
+function connectLegend(theLegend) {
+    _globalLegend = theLegend;
+}
+
+function connectControl(theControl) {
+    _globalControl = theControl;
+}
+
+function setFocusLayer(theLayer) {
+    _focusLayer = theLayer;
+}
+// VIC Basemaps:
+//CARTO_VG CARTO_OVERLAY_VG AERIAL_VG
+function baseVicWms(name) {
+    var baseVic = "https://base.maps.vic.gov.au/service?";
+    var wmsLayer = L.tileLayer.wms(baseVic, {
+        dpiMode: '7',
+        featureCount: '10',
+        format: 'image/png',
+        layers: name,
+        transparent: true,
+    });
+    return wmsLayer;
+}
+
+
+function freshenLeaflet(freshmap) {
+    var recall = freshmap;
+    freshmap.invalidateSize();
+    _focusLayer.bringToFront();
+    setTimeout(function () { freshenLeaflet(recall); }, 750);
+}
+
+function legendStyle(desc, symbol) {
+    if (typeof legendStyle.counter == 'undefined') {
+        legendStyle.counter = 0;
+    }
+    if (!(desc in _legendList)) {
+        _legendList[desc] = symbol.options;
+        var applying = _legendList[desc];
+        legendStyle.counter++;
+        applying.locy = ((2 - legendStyle.counter) * 25);
+        // var marker = new L.marker([(legendStyle.counter*20),0], { opacity: 0.01 }); //opacity may be set to zero
+        console.log(legendStyle.counter + ":" + applying.locy);
+        var blot = new L.circleMarker([applying.locy, 10], {
+            radius: applying.radius, //8,
+            fillColor: applying.fillColor, //"#CA6FA8",
+            color: applying.color, //"#AA78B0",
+            weight: applying.weight, //1,
+            opacity: applying.opacity, //1,
+            fillOpacity: applying.fillOpacity, //0.3,
+        });
+        blot.bindTooltip(desc, { permanent: true, className: "legend-label", offset: [10, 0], opacity: 0.85 });
+        // marker.addTo(legend);
+        blot.addTo(_globalLegend);
+    }
+    return symbol;
+}
+
+function getWFS(URL, theMap, _symStyles, _symPoints, _popupFilters, addto, listed) {
+    addto = addto ?? true;
+    var WFSLayer = null;
+
+    fetch(URL).then(function (response) {
+        response.json().then(function (lyr) {
+            WFSLayer = L.geoJson(lyr, {
+                style: function (feature) {
+                    for (var s = 0; s < _symStyles.length; s++) {
+                        styled = _symStyles[s]._symFilter(feature);
+                        if (styled) {
+                            return _symStyles[s].symbology;
+                        }
+                    }
+                },
+                pointToLayer: function (feature, latlng) {
+                    for (var p = 0; p < _symPoints.length; p++) {
+                        pointed = _symPoints[p]._symFilter(feature);
+                        if (pointed) {
+                            return legendStyle(
+                                pointed,
+                                L.circleMarker(latlng, _symPoints[p].symbology)
+                            );
+                        }
+                    }
+                }
+
+            }).bindPopup(function (layer) {
+                return _popupFilters(layer);
+            }) // .addTo(theMap);
+        })
+            .then(function () { 
+                if(listed) {
+                    _globalControl.addOverlay(WFSLayer, listed);
+                }
+                if(addto) {
+                WFSLayer.addTo(theMap);
+                }
+                return WFSLayer; });
+    });
+}
+
+
+//////////////////////////////////////////////////////
+/*
+// https://gis.stackexchange.com/questions/169129/how-to-add-a-bounding-box-filter-to-this-leaflet-wfs-request
+ 
+Simply use map.getBounds(), maybe with a little bit of padding.
+Once you have that latLngBounds, you could try the toBBoxString()
+as value for your "bbox" parameter.
+Then on map move end event re-send your request and replace your geoJson layer by the new data.
+*/
+//////////////////////////////////////////////////////
+
+// eg:     myLimitBox = hangEdges(mymap.getBounds()).toBBoxString();
+
+function hangEdges(myBounds, pad) {
+    pad = pad ?? 1;
+    console.log(myBounds);
+    var myMid = {
+        lat: (myBounds._northEast.lat + myBounds._southWest.lat) / 2,
+        lng: (myBounds._northEast.lng + myBounds._southWest.lng) / 2,
+    };
+    var ne = L.latLng(
+        myMid.lat + ((myBounds._northEast.lat - myMid.lat) * pad),
+        myMid.lng + ((myBounds._northEast.lng - myMid.lng) * pad)
+    );
+    var sw = L.latLng(
+        myMid.lat + ((myBounds._southWest.lat - myMid.lat) * pad),
+        myMid.lng + ((myBounds._southWest.lng - myMid.lng) * pad)
+    );
+    myStretch = L.latLngBounds(ne, sw);
+    console.log(myStretch);
+    return myStretch;
+}
+
+    // // // pointToLayer: function (feature, latlng) {
+    // // //     //     return L.marker(latlng, {
+    // // //     //         // icon
+    // // //     //         // keyboard
+    // // //     //         // title
+    // // //     //         // alt
+    // // //     //         // zIndexOffset
+    // // //     //         // opacity
+    // // //     //         // riseOnHover
+    // // //     //         // riseOffset
+    // // //     //         // pane
+    // // //     //         // shadowPane
+    // // //     //     });
+    // // //     // },
+    // // //     return L.circleMarker(latlng, {
+    // // //     });
+    // // // }
+
+    // // //             style: function (feature) {
+    // // //                 return {
+    // // //                 };
+    // // //                 // {
+    // // //                 // stroke	Boolean	true	Whether to draw stroke along the path. Set it to false to disable borders on polygons or circles.
+    // // //                 // color	String	'#3388ff'	Stroke color
+    // // //                 // weight	Number	3	Stroke width in pixels
+    // // //                 // opacity	Number	1.0	Stroke opacity
+    // // //                 // lineCap	String	'round'	A string that defines shape to be used at the end of the stroke.
+    // // //                 // lineJoin	String	'round'	A string that defines shape to be used at the corners of the stroke.
+    // // //                 // dashArray	String	null	A string that defines the stroke dash pattern. Doesn't work on Canvas-powered layers in some old browsers.
+    // // //                 // dashOffset	String	null	A string that defines the distance into the dash pattern to start the dash. Doesn't work on Canvas-powered layers in some old browsers.
+    // // //                 // fill	Boolean	depends	Whether to fill the path with color. Set it to false to disable filling on polygons or circles.
+    // // //                 // fillColor	String	*	Fill color. Defaults to the value of the color option
+    // // //                 // fillOpacity	Number	0.2	Fill opacity.
+    // // //                 // };
+    // // //             }
+
---
+++ assets/js/mapping/waterfalls.js
@@ -0,0 +1,260 @@
+
+
+
+
+function pageLaunch() {
+
+    var mymap = L.map('lf-map').setView([-37.56655532524054, 149.1559270001807], 11);
+    var legend = L.map('legend', { zoomControl: false, attributionControl: false, dragging: false }).setView([0, 300], 1);
+
+    connectMap(mymap);
+    connectLegend(legend);
+
+    var carto = baseVicWms("CARTO_VG").addTo(mymap);
+    var photo = baseVicWms("AERIAL_VG");
+    var ovrly = baseVicWms("CARTO_OVERLAY_VG");
+    
+    var compo = L.layerGroup([photo,ovrly]);
+
+    var baseMaps = {
+        "Basemap": carto,
+        "Image": compo,
+    };
+
+    var popControl = L.control.layers(baseMaps);
+    connectControl(popControl);
+
+    popControl.addTo(mymap);
+    setFocusLayer(L.tileLayer(''));
+    freshenLeaflet(mymap);
+
+    var _polyInterest = {
+        symbology: {
+            fillColor: "#930eab", //EAAFD8",
+            color: "#edbbcd", //FAEFA8",
+            weight: 0.5,
+            opacity: 1,
+            fillOpacity: 0.5,
+        },
+        _symFilter: function (feature) { return null; }
+    };
+
+    var _symInterest = {};
+    Object.assign(_symInterest, _polyInterest);
+    _symInterest.radius = 8;
+
+    var _symObject = {
+        symbology: {
+            radius: 4,
+            fillColor: "#F49F68",
+            color: "#F4CF78",
+            weight: 2,
+            opacity: 1,
+            fillOpacity: 0.8,
+        },
+        _symFilter: function (feature) { return null; }
+    };
+
+    _polyInterest._symFilter = function (feature) {
+        if (feature.properties.ACT) { //GENER_TYPE == '1') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    _symInterest._symFilter = function (feature) {
+        if (
+            (feature.properties.FAC_TYPE == "REC SITES")
+        ) {
+            return "Recreation and Nature";
+        }
+        return null;
+    }
+
+    _symObject._symFilter = function (feature) {
+        var generalDescriptor = "Installed Structure";
+        if (feature.properties.ASSET_CLS !== 'SITE ELEMENT' ||
+            feature.properties.CATEGORY == 'Gate' ||
+            feature.properties.CATEGORY == 'Bench - Seat') {
+            return generalDescriptor;
+        }  
+        var structures = [
+            'PICNIC SHELTER',
+            'TOILET BLOCK',
+            'VIEWING PLATFORM',
+            'JETTY',
+            'INFORMATION SHELTER',
+        ];
+        classed = feature.properties.ASSET_CLS;
+        if (
+            (classed && structures.includes(classed))
+        ) {
+            return generalDescriptor;
+        }
+        return null;
+    }
+
+    var _symStyles = [_polyInterest];
+    var _symPoints = [_symInterest, _symObject];
+    var _popupFilters = function (layer) {
+        if (layer.feature.properties.ACT) {
+            if ((layer.feature.properties.LABEL ?? " ") == " ") {
+                return layer.feature.properties.NAME;
+            } else {
+                return layer.feature.properties.LABEL;
+            }
+        }
+        if (layer.feature.properties.FAC_TYPE == "REC SITES") {
+            captioned = layer.feature.properties.LABEL ?? "Site of interest.";
+            detailed = layer.feature.properties.COMMENTS ?? "";
+            if (captioned && detailed) {
+                captioned += ": " + detailed;
+            }
+            return captioned;
+        }
+        if (layer.feature.properties.ASSET_CLS == 'SITE ELEMENT' &&
+            (layer.feature.properties.CATEGORY == 'Gate' ||
+                layer.feature.properties.CATEGORY == 'Bench - Seat')) {
+            return layer.feature.properties.CATEGORY;
+        }
+        return layer.feature.properties.ASSET_CLS;
+    }
+
+    var layerParameters = {
+        service: 'WFS',
+        version: '2.0.0',
+        request: 'GetFeature',
+        maxFeatures: 200,
+        outputFormat: 'application/json',
+    };
+
+    var rootUrl = 'https://services.land.vic.gov.au/catalogue/publicproxy/guest/dv_geoserver/wfs?';
+
+    layerParameters.typeName = 'datavic:FORESTS_RECWEB_ASSET';
+    var URL = rootUrl + L.Util.getParamString(layerParameters);
+    var l1 = getWFS(URL, mymap, _symStyles, _symPoints, _popupFilters);
+    layerParameters.typeName = 'datavic:FORESTS_RECWEB_SITE';
+    var URL = rootUrl + L.Util.getParamString(layerParameters);
+    var l2 = getWFS(URL, mymap, _symStyles, _symPoints, _popupFilters);
+
+    layerParameters.typeName = 'datavic:CROWNLAND_PLM25_H_A_C_FEAT_RES';
+    var URL = rootUrl + L.Util.getParamString(layerParameters);
+    var l3 = getWFS(URL, mymap, _symStyles, _symPoints, _popupFilters, false, "Historical");
+    layerParameters.typeName = 'datavic:CROWNLAND_PLM25_COMM_USE_AREA';
+    var URL = rootUrl + L.Util.getParamString(layerParameters);
+    var l4 = getWFS(URL, mymap, _symStyles, _symPoints, _popupFilters, false, "Community");
+
+    layerParameters.typeName = 'datavic:CROWNLAND_PLM25_FOREST_PARK';
+    var URL = rootUrl + L.Util.getParamString(layerParameters);
+    var l5 = getWFS(URL, mymap, _symStyles, _symPoints, _popupFilters, false, "Forest parks");
+
+    layerParameters.typeName = 'datavic:CROWNLAND_PLM25_NATURAL_FEAT';
+    var URL = rootUrl + L.Util.getParamString(layerParameters);
+    var l6 = getWFS(URL, mymap, _symStyles, _symPoints, _popupFilters, false, "Landscape");
+    layerParameters.typeName = 'datavic:CROWNLAND_PLM25_NATURE_CONSERV';
+    var URL = rootUrl + L.Util.getParamString(layerParameters);
+    var l7 = getWFS(URL, mymap, _symStyles, _symPoints, _popupFilters, false, "Conservation");
+    
+    layerParameters.typeName = 'datavic:CROWNLAND_PLM25_ALPINE_RESORT';
+    var URL = rootUrl + L.Util.getParamString(layerParameters);
+    var l8 = getWFS(URL, mymap, _symStyles, _symPoints, _popupFilters, false, "Resorts");
+
+    layerParameters.typeName = 'datavic:CROWNLAND_PLM25_COASTAL_RES';
+    var URL = rootUrl + L.Util.getParamString(layerParameters);
+    var l9 = getWFS(URL, mymap, _symStyles, _symPoints, _popupFilters, false, "Coastal");
+
+    
+    // var comm = L.layerGroup([l3,l4]);
+    // var parks = L.layerGroup([l5,l8]);
+    // var natural = L.layerGroup([l6,l7]);
+    // var coastal = L.layerGroup([l9]);
+    var topMaps = {
+        // "History and community": comm,
+        // "Parks and resorts": parks,
+        // "Conservation": natural,
+        // "Coastal": coastal,
+    };
+
+    ////////////////////////////////////////////////
+    ////////////////////////////////////////////////
+
+    var layerParameters = {
+        service: 'WFS',
+        version: '2.0.0',
+        request: 'GetFeature',
+        typeName: 'datavic:VMHYDRO_WATERFALL',
+        // typeName: 'datavic:FORESTS_OG100', 
+        //, ,datavic:FORESTS_OG100 datavic:FLORAFAUNA1_NV2005_EVCBCS_9_1',datavic:FORESTS_RECWEB_POI
+        maxFeatures: 200,
+        outputFormat: 'application/json',
+    };
+
+    var parameters = L.Util.extend(layerParameters);
+    var URL = rootUrl + L.Util.getParamString(parameters);
+
+    var WFSLayer = null;
+    fetch(URL).then(function (response) {
+        response.json().then(function (lyr) {
+            WFSLayer = L.geoJson(lyr, {
+
+                pointToLayer: function (feature, latlng) {
+                    //     return L.marker(latlng, {
+                    //         // icon
+                    //         // keyboard
+                    //         // title
+                    //         // alt
+                    //         // zIndexOffset
+                    //         // opacity
+                    //         // riseOnHover
+                    //         // riseOffset
+                    //         // pane
+                    //         // shadowPane
+                    //     });
+                    // },
+                    return legendStyle("Waterfall",
+                        L.circleMarker(latlng, {
+                            radius: 4,
+                            fillColor: "#6AF880",
+                            color: "#4AcF68",
+                            weight: 3,
+                            opacity: 1,
+                            fillOpacity: 0.25,
+                        })
+                    );
+                }
+
+            }).bindPopup(function (layer) {
+                if (layer.feature.properties.NAME) {
+                    return layer.feature.properties.NAME;
+                } else {
+                    return "Waterfalls";
+                }
+            }).addTo(mymap);
+        })
+        .then(function(){ 
+            setFocusLayer(WFSLayer);
+        });
+    });
+
+
+    /*Legend specific*/
+
+    //////////////////////////////////////////////////////
+
+    // var wmsLayer = L.tileLayer.wms("http://services.land.vic.gov.au/catalogue/publicproxy/guest/dv_geoserver/datavic/ows?", {
+    //   dpiMode: '7',
+    //   featureCount: '10',
+    //   layers: 'VMTRANS_TR_ROAD',
+    //     format: 'image/png',
+    //     transparent: 'true',
+    // }).addTo(mymap);
+    // var wmsLayer = L.tileLayer.wms(", {
+    //   dpiMode: '7',
+    //   featureCount: '10',
+    //   layers: 'FLORAFAUNA1_NV2005_EVCBCS_9_1,FLORAFAUNA1_NV1750_EVCBCS_9_1',
+    //     format: 'image/png',
+    //     transparent: 'true',
+    // }).addTo(mymap);
+
+}
